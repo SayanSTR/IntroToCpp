0f077840f6acf66830d7179ff61cfc17a0b5b039
SayanSTR <sayan02051996play@gmail.com>
2021-01-12 20:55:12 +0530

    name change

---
+++ Solutions/ch01/q1.9.cpp
@@ -0,0 +1,11 @@
+/* 9. When you hold a set of cards in your hand, you usually arrange them fanned 
+ * out. Say you start with cards stacked one on top of the other. Then you
+ * rotate the ith card from the top by an amount proportional to i (say 10i
+ * degrees to the left) around the bottom left corner. Now, we can see the top
+ * card completely, but the other cards are seen only partially. In particular,
+ * only a triangular portion of each card is seen, with the top left corner
+ * being at the apex of each triangle. This is the figure that you are to draw.
+ * (a)Draw it assuming the cards are transparent. (b)Draw it assuming the cards
+ * are opaque. For this some trigonometric calculation will be necessary.
+ * In both cases, use repeat statements to keep your program small as possible.
+ */
---
+++ Solutions/ch04/q4.1.cpp
@@ -0,0 +1,6 @@
+/* 1. Write a program to compute the value of
+ * D(r) = Sum[k=0 to r] (-1^k).( r!/k!)
+ * Incidentally, D (r ) is the number of ways in which the numbers 1 through r
+ * can be arranged in a sequence such that i is never in the ith position, for
+ * all i.
+ */
