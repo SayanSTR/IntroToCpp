8d82e073fc86793da2e6adeb7996578131d75b47
SayanSTR <sayan02051996play@gmail.com>
2021-01-14 18:48:49 +0530

    Added question to q4.1.cpp

---
+++ Solutions/ch04/q4.2.cpp
@@ -0,0 +1,8 @@
+/* 2. Here is an infinite product which can be shown to approach 2/pi as the number of terms
+ * increases.
+ * 2/pi =sqrt(2)/2.sqrt(2+sqrt(2))/2.sqrt(2+sqrt(2+sqrt(2)))/2...
+ * Write a program that computes the product of the first n terms, where n is specifed as 
+ * input. You will need to specify what values your variables take after some t iterations.
+ * For this feel free to write something like "numerator has value sqrt(2 + sqrt(...+ sqrt(2))... with
+ * sqrt() appearing t times". Write a proof of correctness.
+ */
