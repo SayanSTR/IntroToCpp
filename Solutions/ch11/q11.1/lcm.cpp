99de33bd0ecd23a32b8ab3202cc8af841aaf96f5
SayanSTR <sayan02051996play@gmail.com>
2021-06-04 15:30:08 +0530

    ..

--- Solutions/ch11/q11.1/lcm.cpp
+++ Solutions/ch11/q11.1/lcm.cpp
@@ -1,4 +1,16 @@
-int gcd(int, int);  // declaration of function gcd
+/*int gcd(int, int);  // declaration of function gcd*/
+
+int gcd(int m, int n){  // definition of gcd
+    int mdash,ndash;
+    while(m % n != 0)
+    {
+        mdash = n;
+        ndash = m % n;
+        m = mdash;
+        n = ndash;
+    }
+    return n;
+}
 
 int lcm(int m, int n){
     return m*n/gcd(m,n);
